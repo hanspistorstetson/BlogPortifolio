cba1c7ccad1aa77c896d125a00208303f6692f42
Hans Pistor <hpistor@stetson.edu>
2017-06-05 13:45:43 -0400

    SSL certificate take 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   end
 
   def letsencrypt
-    render text: "NXRbxitL5I9Rq9o5g8eSUcle17J8t8-sad7cbCTOCw4.kJWS4XEFw6ycpWSA5mQitaZWN_K5cSUeYyolceKs7RA"
+    render text: "FdXUxLZdge2iClkDamrPWaH-XbdaTKPPE7gbGfuYzAE.kJWS4XEFw6ycpWSA5mQitaZWN_K5cSUeYyolceKs7RA"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
-  get '/.well-known/acme-challenge/NXRbxitL5I9Rq9o5g8eSUcle17J8t8-sad7cbCTOCw4 ' => 'welcome#letsencrypt'
+  get '/.well-known/acme-challenge/:id' => 'welcome#letsencrypt'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
