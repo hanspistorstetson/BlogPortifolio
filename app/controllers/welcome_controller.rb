cba4693f8e80b308fc9a8edc10b18652ef3b913a
Hans Pistor <hpistor@stetson.edu>
2017-06-05 13:36:58 -0400

    SSL Certificate

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def letsencrypt
+    render text: "NXRbxitL5I9Rq9o5g8eSUcle17J8t8-sad7cbCTOCw4.kJWS4XEFw6ycpWSA5mQitaZWN_K5cSUeYyolceKs7RA"
+  end
+
 end
