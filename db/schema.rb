d1bb4dda3da17cf7f1f52e3802ace86c05d25157
Hans Pistor <hpistor@stetson.edu>
2017-06-06 14:29:05 -0400

    Fixed link styling, working on projects, etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606164841) do
+ActiveRecord::Schema.define(version: 20170606173759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20170606164841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.string "projecturl"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
