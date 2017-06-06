3f47ef165af4c542e4a00baddc51480abb721b1b
Hans Pistor <hpistor@stetson.edu>
2017-06-05 22:58:12 -0400

    Added post show page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @posts = Post.all.limit(3).order("created_at desc")
   end
-
-  def articles
-
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   resources :posts
   get 'welcome/index'
-  get 'welcome/articles'
   root 'welcome#index'
-  get '/.well-known/acme-challenge/:id' => 'welcome#letsencrypt'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
