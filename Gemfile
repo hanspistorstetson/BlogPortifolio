1be34b8a1de44409bb76f5b67e807977ab0aa5ec
Hans Pistor <hpistor@stetson.edu>
2017-06-06 20:13:51 -0400

    added pagination to posts

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'pg'
 gem 'pygments.rb'
 gem 'redcarpet', '~> 3.0.0'
 gem 'devise'
+gem 'will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_admin!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @posts = Post.all.order("created_at desc")
+    @posts = Post.all.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
