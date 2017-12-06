5f43389f5bbc272c0a7099bc9d4cf8f445c5d21a
Hans Pistor <hpistor@stetson.edu>
2017-12-06 13:28:01 -0500

    added rouge color syntax highlighting

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 gem 'sitemap_generator'
+gem 'rouge'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  require 'redcarpet'
+  require 'rouge'
+  require 'rouge/plugins/redcarpet'
+
+  class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+  end
+
+
   def markdown(content)
 
     options = {
